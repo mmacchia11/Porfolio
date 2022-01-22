187c4369541babd11eb5e5b3c2d54ea9938408ef
Martin Macchia <mmacchia1178@gmail.com>
2022-01-22 20:00:51 -0300

    realice la parde servicios

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,30 @@
 /*==================== MENU MOSTRAR Y OCULTAR====================*/
-const navMenu =  document.getElementById('nav-menu')
-      navToggle = document.getElementById('nav-toggle')  
-      navClose = document.getElementById('nav-close')  
+const navMenu = document.getElementById('nav-menu')
+navToggle = document.getElementById('nav-toggle')
+navClose = document.getElementById('nav-close')
 /*===== MENU MOSTRAR =====*/
-if(navToggle){
-        navToggle.addEventListener('click',()=>{
+if (navToggle) {
+        navToggle.addEventListener('click', () => {
                 navMenu.classList.add('show-menu')
         })
 }
 
 
 /*===== MENU OCUALTAR =====*/
-if(navClose){
-     navClose.addEventListener('click',()=>{
-             navMenu.classList.remove('show-menu')
-     })   
+if (navClose) {
+        navClose.addEventListener('click', () => {
+                navMenu.classList.remove('show-menu')
+        })
 }
 
 
 /*==================== SACAR MENU CEL ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // si hacen click en cualquir item, se remueve show-menu
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+        const navMenu = document.getElementById('nav-menu')
+        // si hacen click en cualquir item, se remueve show-menu
+        navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -37,27 +37,67 @@ const skillsContent = document.getElementsByClassName('skills__content')
 const skillsHeader = document.querySelectorAll('.skills__header')
 
 
-function toggleSkills(){
-       let itemClass = this.parentNode.className
+function toggleSkills() {
+        let itemClass = this.parentNode.className
 
-       for(i=0; i< skillsContent.length;i++){
-               skillsContent[i].className='skills__content skills__close'
-       }
+        for (i = 0; i < skillsContent.length; i++) {
+                skillsContent[i].className = 'skills__content skills__close'
+        }
 
-       if(itemClass === 'skills__content skills__close'){
-               this.parentNode.className = 'skills__content skills__open'
-       }
+        if (itemClass === 'skills__content skills__close') {
+                this.parentNode.className = 'skills__content skills__open'
+        }
 }
 
 skillsHeader.forEach((el) => {
-        el.addEventListener('click',toggleSkills)
+        el.addEventListener('click', toggleSkills)
 })
 
 /*==================== CAULIDADES  ====================*/
 
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+                const target = document.querySelector(tab.dataset.target)
+
+                tabContents.forEach(tabContent => {
+                        tabContent.classList.remove('qualification__active')
+                })
+
+
+                target.classList.add('qualification__active')
+                tabs.forEach(tab => {
+                        tab.classList.remove('qualification__active')
+                })
+
+                tab.classList.add('qualification__active')
+        })
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')  
 
+let modal = function(modalClick){
+        modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i) =>{
+        modalBtn.addEventListener('click',()=>{
+                modal(i)
+        })
+})
+
+modalCloses.forEach((modalClose) => {
+        modalClose.addEventListener('click',() => {
+                modalViews.forEach((modalView) => {
+                        modalView.classList.remove('active-modal')
+                })
+        })
+})
 
 /*==================== PORTFOLIO   ====================*/
 
@@ -68,10 +108,10 @@ skillsHeader.forEach((el) => {
 /*==================== SECCION SCROLL  ACTIVA LINK ====================*/
 
 
-/*==================== CAMBIAR COLOR FONDO HEADER ====================*/ 
+/*==================== CAMBIAR COLOR FONDO HEADER ====================*/
 
 
-/*==================== MOSTRAR SCROLL UP ====================*/ 
+/*==================== MOSTRAR SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
